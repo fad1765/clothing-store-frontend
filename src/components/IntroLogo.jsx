f95e3a37a7dcf1c6c927daac48871e16d18d12a1
fad1765 <qaq70326@gmail.com>
2026-04-05 20:19:37 +0800

    update:修改檔名

--- src/components/IntroLogo.jsx
+++ src/components/IntroLogo.jsx
@@ -1,6 +1,6 @@
 import { motion, AnimatePresence } from "framer-motion";
 import { useEffect } from "react";
-import "../styles/IntroLogo.css";
+import "../styles/introLogo.css";
 
 const logoImage = "/images/logo.jpg";
 
